b517ce8a36204af3ee39f3cebabd9b3e7ea6c586
Jim Kroon <info@jimkroon.nl>
2024-05-28 21:37:34 +0200

    Lint fixes

--- src/input/gamepad.ts
+++ src/input/gamepad.ts
@@ -359,7 +359,7 @@ export default class Gamepad {
         }
 
         if(this._options.enable_vibration === false){
-            return;
+            return
         }
 
         const rumbleData = {
